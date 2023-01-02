b8a0c95d072380abdccbc62c512cb6f69d864c7c
Olivier CROGUENNEC <olivier.croguennec@bpce-it.fr>
2023-01-02 22:49:14 +0100

    Add Logger Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { ShareModule } from "./share/share.module";
 import { UserModule } from "./user/user.module";
 import { JobsModule } from "./jobs/jobs.module";
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
@@ -48,6 +49,7 @@ import { JobsModule } from "./jobs/jobs.module";
       limit: 100,
     }),
     ScheduleModule.forRoot(),
+    LoggerModule,
   ],
 })
 export class AppModule {}
