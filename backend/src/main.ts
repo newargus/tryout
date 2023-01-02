0be3edc948779dd0c4b1509ae341ef592e52d878
Olivier CROGUENNEC <olivier.croguennec@bpce-it.fr>
2023-01-02 22:47:34 +0100

    Logger + Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as fs from "fs";
 import { AppModule } from "./app.module";
+import { LoggerInterceptor } from "./logger/logger.interceptor";
+import { LoggerService } from "./logger/logger.service";
 
 async function bootstrap() {
+  const env = process.env.NODE_ENV;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor(new LoggerService))
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.set("trust proxy", true);
@@ -14,6 +19,21 @@ async function bootstrap() {
   await fs.promises.mkdir("./data/uploads/_temp", { recursive: true });
 
   app.setGlobalPrefix("api");
+// swagger config
+if (env !== 'production') {
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nestjs API')
+    .setDescription('Example Swagger')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [],
+    deepScanRoutes: true,
+  });
+  SwaggerModule.setup('api', app, document);
+}
+
   await app.listen(8080);
 }
 bootstrap();
